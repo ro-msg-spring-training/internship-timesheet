71233b633e35e65725021c6f1b554b8c26b6bf51
PATRICIA TRUTA <georgianatruta.pt@gmail.com>
2020-05-13 16:58:38 +0300

    getPsps with PspDto in ProgramController

--- timesheet/src/main/java/ro/msg/internship/timesheet/controller/ProgramController.java
+++ timesheet/src/main/java/ro/msg/internship/timesheet/controller/ProgramController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import ro.msg.internship.timesheet.dto.PspDto;
+import ro.msg.internship.timesheet.dto.builder.PspBuilder;
+import ro.msg.internship.timesheet.model.Psp;
 import ro.msg.internship.timesheet.service.ProgramService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @CrossOrigin("*")
 
@@ -19,7 +24,17 @@ public class ProgramController {
     private final ProgramService programService;
 
     @GetMapping("/programs/{programName}/psps")
-    public ResponseEntity<List<String>> getPsps(@PathVariable String programName) {
-        return ResponseEntity.accepted().body(programService.getPsps(programName));
+    public ResponseEntity<List<PspDto>> getPsps(@PathVariable String programName) {
+
+        Set<Psp> psps = programService.getPsps(programName);
+
+        List<PspDto> pspDtos = new ArrayList<>();
+
+        for (Psp psp : psps) {
+            PspDto pspDto = PspBuilder.getDtoFromEntity(psp);
+            pspDtos.add(pspDto);
+        }
+
+        return ResponseEntity.accepted().body(pspDtos);
     }
 }
--- timesheet/src/main/java/ro/msg/internship/timesheet/service/ProgramService.java
+++ timesheet/src/main/java/ro/msg/internship/timesheet/service/ProgramService.java
@@ -4,45 +4,42 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ro.msg.internship.timesheet.exception.ProgramNotFoundException;
 import ro.msg.internship.timesheet.model.Program;
+import ro.msg.internship.timesheet.model.Psp;
 import ro.msg.internship.timesheet.repository.ProgramRepository;
 
 import javax.transaction.Transactional;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class ProgramService {
-	private final ProgramRepository programRepository;
+    private final ProgramRepository programRepository;
 
-	public Program getProgramById(Integer id) {
-		return programRepository.findById(id).orElseThrow(ProgramNotFoundException::new);
-	}
+    public Program getProgramById(Integer id) {
+        return programRepository.findById(id).orElseThrow(ProgramNotFoundException::new);
+    }
 
-	public Program createProgram(Program program) {
-		return programRepository.save(program);
-	}
+    public Program createProgram(Program program) {
+        return programRepository.save(program);
+    }
 
-	public List<Program> getPrograms() {
-		return programRepository.findAll();
-	}
+    public List<Program> getPrograms() {
+        return programRepository.findAll();
+    }
 
-	@Transactional
-	public void deleteAll() {
-		programRepository.deleteAll();
-	}
+    @Transactional
+    public void deleteAll() {
+        programRepository.deleteAll();
+    }
 
-	public Program getProgramByName(String name) {
-		return programRepository.findByName(name).orElseThrow(ProgramNotFoundException::new);
-	}
+    public Program getProgramByName(String name) {
+        return programRepository.findByName(name).orElseThrow(ProgramNotFoundException::new);
+    }
 
-	public List<String> getPsps(String name) {
-		Program program = getProgramByName(name);
-		List<String> pspNames = new ArrayList<>();
+    public Set<Psp> getPsps(String name) {
+        Program program = getProgramByName(name);
 
-		program.getPsps().forEach(psp -> pspNames.add(psp.getName()));
-
-		return pspNames;
-	}
+        return program.getPsps();
+    }
 }
