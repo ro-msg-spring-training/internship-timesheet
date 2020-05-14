f9ed9e819ec1978e614a09e79141f76ce5d361a4
Somcutean Alina <alinasomcutean@yahoo.com>
2020-05-14 10:58:20 +0300

    Updated security for login

--- timesheet/src/main/java/ro/msg/internship/timesheet/configuration/FormSecurityConfiguration.java
+++ timesheet/src/main/java/ro/msg/internship/timesheet/configuration/FormSecurityConfiguration.java
@@ -1,42 +1,55 @@
 package ro.msg.internship.timesheet.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ro.msg.internship.timesheet.model.User;
+import ro.msg.internship.timesheet.service.UserDetailService;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
-@Profile("with-form")
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class FormSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private final UserDetailsService userDetailsService;
+    private final UserDetailService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(User.PASSWORD_ENCODER);
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
+        http.csrf()
+                .disable()
+                .cors()
+                .configurationSource(corsConfigurationSource()).and()
+                .authorizeRequests()
+                .antMatchers("/loginUser").permitAll()
                 .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .permitAll()
-                .and()
-                .csrf().disable();
-        http.headers().frameOptions().disable();
+            .formLogin()
+                .loginProcessingUrl("/loginUser")
+                .loginPage("/loginUser").permitAll()
+                .defaultSuccessUrl("/users");
     }
 
 }
--- timesheet/src/main/java/ro/msg/internship/timesheet/controller/LoginController.java
+++ timesheet/src/main/java/ro/msg/internship/timesheet/controller/LoginController.java
@@ -3,16 +3,14 @@ package ro.msg.internship.timesheet.controller;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ro.msg.internship.timesheet.dto.UserDto;
 import ro.msg.internship.timesheet.dto.builder.UserBuilder;
 import ro.msg.internship.timesheet.model.User;
 import ro.msg.internship.timesheet.service.LoginService;
-
-@CrossOrigin("*")
 
 @RequiredArgsConstructor
 @RestController
@@ -20,13 +18,11 @@ public class LoginController {
 
     private final LoginService loginService;
 
-    @GetMapping("/loginUser")
+    @RequestMapping(value = "/loginUser", method = RequestMethod.POST)
     public ResponseEntity<UserDto> loginUser(@RequestBody UserDto user) {
-
         User loggedUser = loginService.loginUser(UserBuilder.getUserFromDto(user));
 
-        return ResponseEntity.accepted().body(UserBuilder.getDtoFromUser(loggedUser));
-
+        return ResponseEntity.ok(UserBuilder.getDtoFromUser(loggedUser));
     }
 
 }
